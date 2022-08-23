e40484c6a897ba6103abf31f8dae3dd14a33f0f8
Holly Borla <hborla@apple.com>
2022-08-22 22:25:02 -0400

    [Sema] Allow registry types as custom attributes on non-generic types and global/static functions.

--- test/type/registry_types.swift
+++ test/type/registry_types.swift
@@ -20,3 +20,43 @@ extension Int {}
 
 @registry // expected-error {{'@registry' attribute cannot be applied to this declaration}}
 var registryVar: Int = 10
+
+@registry
+struct ConformanceCollector {}
+
+@ConformanceCollector
+protocol P {}
+
+@ConformanceCollector
+struct ConformingStruct: P {}
+
+@ConformanceCollector
+struct GenericStruct<T>: P {}
+  // expected-error@-1 {{Cannot register generic struct 'GenericStruct' with registry 'ConformanceCollector'}}
+
+@ConformanceCollector
+extension Int: P {}
+// expected-error@-1 {{Registry type 'ConformanceCollector' can only be applied to non-generic types and top-level functions}}
+
+@registry
+struct FunctionCollector {}
+
+@FunctionCollector
+func global() {}
+
+@FunctionCollector
+func globalGeneric<T>(value: T) {}
+// expected-error@-1 {{Cannot register global function 'globalGeneric' with registry 'FunctionCollector'}}
+
+struct TypeContext {
+  @FunctionCollector
+  func instanceMethod() {}
+  // expected-error@-1 {{Cannot register instance method 'instanceMethod' with registry 'FunctionCollector'}}
+
+  @FunctionCollector
+  static func staticMethod() {}
+
+  @FunctionCollector
+  let property: Int
+  // expected-error@-1 {{Cannot register property 'property' with registry 'FunctionCollector'}}
+}
