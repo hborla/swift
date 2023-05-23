986e54fcc9a3d53a8083d2ccd7035bde33b41231
Holly Borla <hborla@apple.com>
2023-05-23 16:52:18 -0700

    [AST] Add 'initializes' and 'accesses' attributes for init accessor definite initialization dependencies.

--- lib/Parse/ParseDecl.cpp
+++ lib/Parse/ParseDecl.cpp
@@ -3294,6 +3294,14 @@ ParserStatus Parser::parseNewDeclAttribute(DeclAttributes &Attributes,
     break;
     }
 
+  case DAK_Initializes: {
+    llvm_unreachable("InitializesAttr not yet implemented");
+  }
+
+  case DAK_Accesses: {
+    llvm_unreachable("AccessesAttr not yet implemented");
+  }
+
   case DAK_Implements: {
     ParserResult<ImplementsAttr> Attr = parseImplementsAttribute(AtLoc, Loc);
     if (Attr.isNonNull()) {
